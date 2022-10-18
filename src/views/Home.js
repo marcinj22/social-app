d6a1d019c710d40f5270abcd7740a7fd200d6cdf
marcinj <marcin.jurewicz@gmail.com>
2022-10-18 14:55:12 +0200

    adding posts

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,5 +1,50 @@
+import './Home.css';
+
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Post from "../components/Post";
+
 const Home = () => {
-    return <h2>Home</h2>;   
+
+    const [posts, setPosts] = useState([])
+
+    const getLatestPosts = () => {
+        axios.post("https://akademia108.pl/api/social-app/post/latest").then((res) => {
+            setPosts(res.data)
+        })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+
+    const getNextPosts = () => {
+        axios.post("https://akademia108.pl/api/social-app/post/older-then", {
+            date: posts[posts.length - 1].created_at
+        })
+        .then((res) => {
+            setPosts(posts.concat(res.data));
+        })
+            // .catch((error) => {
+            //     console.log(error);
+            // });
+    };
+
+    useEffect(() => {
+        getLatestPosts();
+    }, []);
+
+    return (
+        <div className="home">
+            <div className="postList">
+                {posts.map((post) => {
+                    return <Post post={post} key={post.id} />;
+                })}
+                <button className="btn loadMore" onClick={getNextPosts}>Load more</button>
+            </div>
+
+        </div>
+
+    );
 }
 
 export default Home;
